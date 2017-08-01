7e7647d114ee73a2c5f402e9e8516829676260c3
waders92 <wade.carlson@pga.com>
2017-07-31 19:51:41 -0500

    pw change

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -5,7 +5,7 @@ var pass1 = prompt('Please Enter Password To Unlock Site: ');
       while (testV < 3) {
       if (!pass1)
       history.go(-1);
-      if (pass1.toLowerCase() == "atlbound" || "mcdezign") {
+      if (pass1.toLowerCase() == "mcdezign") {
       // alert('You Got it Right!');
       window.open('portfolio.html', '_self');
       break;
